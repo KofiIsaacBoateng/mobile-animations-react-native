152ff56e2d156fbeb6f1cb902a919d84db0f723e
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-02-14 14:48:31 +0000

    update: added assets for module 2

---
+++ gestures/basic-01/assets/updates.js
@@ -0,0 +1,93 @@
+export default [
+    {
+        username: "spidee303",
+        name: "Peter Parker",
+        id: Math.random() * 100,
+        profilePhotoUrl: "../assets/imgs/img-01.jpg",
+        stories: [
+            {
+                storyType: "video",
+                source: "../assets/videos/mb-01.mp4"
+            },
+
+            {
+                storyType: "video",
+                source: "../assets/videos/mb-03.mp4"
+            },
+
+            {
+                storyType: "image",
+                source: "../assets/imgs/story-00.jpg"
+            },
+        ]
+    },
+
+    {
+        username: "whereis_myhammer",
+        name: "Thor Odenson",
+        id: Math.random() * 100,
+        profilePhotoUrl: "../assets/imgs/img-04.jpg",
+        stories: [
+            {
+                storyType: "video",
+                source: "../assets/videos/mb-02.mp4"
+            },
+
+            {
+                storyType: "image",
+                source: "../assets/imgs/story-01.jpg"
+            },
+
+            {
+                storyType: "video",
+                source: "../assets/videos/mb-06.mp4"
+            },
+        ]
+    },
+
+    {
+        username: "iron666",
+        name: "Tony Stark",
+        id: Math.random() * 100,
+        profilePhotoUrl: "../assets/imgs/img-05.jpg",
+        stories: [
+            {
+                storyType: "image",
+                source: "../assets/imgs/img-02.jpg"
+            },
+
+            {
+                storyType: "video",
+                source: "../assets/imgs/story-02.jpg"
+            },
+
+            {
+                storyType: "image",
+                source: "../assets/videos/mb-03.mp4"
+            },
+        ]
+    },
+
+    {
+        username: "ohmycaptain",
+        name: "Steve Rogers",
+        id: Math.random() * 100,
+        profilePhotoUrl: "../assets/imgs/img-03.jpg",
+        stories: [
+            {
+                storyType: "video",
+                source: "../assets/videos/mb-04.mp4"
+            },
+
+            {
+                storyType: "image",
+                source: "../assets/imgs/story-03.jpg"
+            },
+
+            {
+                storyType: "image",
+                source: "../assets/videos/mb-05.mp4"
+            },
+        ]
+    }
+]
---
+++ gestures/basic-01/assets/videos.js
@@ -0,0 +1,9 @@
+export default [
+    "../assets/videos/mb-01.mp4",
+    "../assets/videos/mb-02.mp4",
+    "../assets/videos/mb-03.mp4",
+    "../assets/videos/mb-04.mp4",
+    "../assets/videos/mb-05.mp4",
+    "../assets/videos/mb-06.mp4",
+    "../assets/videos/mb-07.mp4",
+]
